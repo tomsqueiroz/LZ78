c8f9e24a8be06a9ab23ef390a16d229799b50f49
tomsqueiroz <toms.queiroz@gmail.com>
2018-04-12 09:43:08 -0300

    Resolver problema funcao add. Ainda há problema om o fato de precisar ler e escrever o mesmo arquivo, no caso, arquivo: compactado.txt

--- compacta.asm
+++ compacta.asm
@@ -1,35 +1,94 @@
 .data
-bufferLeituraArquivo:	.space	1
+fileCompactadoName:	.asciiz	"file.txt"
+fileOriginalName:	.asciiz	"original.txt"
+stringErroArquivo:	.asciiz	"Erro abertura de arquivo"
+inputBuffer:		.space	1
+outputBuffer:		.space	1
 stringLida:		.space	64
 stringValor:		.space	64
 stringChave:		.space	64
-outputBuffer:		.space	1
-fileCompactadoName:	.asciiz	"compactado.txt"
-#NAO ESSENCIAL
-string:			.asciiz	"Ribamar"
-stringValue:		.asciiz	"22"
-fileOriginalName:	.asciiz	"original.txt"
-stringErroArquivo:	.asciiz	"Erro abertura de arquivo"
-
-
+stringChaveEscrever:	.space	64
+stringValorEscrever:	.space	64
+stringPadraoZero:	.space	2
 .text
-la	$a0,string
-jal	retornaIndex
-move	$a0,$v0
-li	$v0,1
+
+li	$v0,13
+la	$a0,fileOriginalName
+li	$a1,0
+li	$a2,0
 syscall
+slt	$t2,$v0,$zero
+	bne	$t2,1,iniciaLeituraCompactar	#se vo > 0, nao ha erro
+	li	$v0,4
+	la	$a0,stringErroArquivo
+	syscall
+	li	$v0,10
+	syscall	
 
+iniciaLeituraCompactar:
+move	$s7,$v0	#filedescriptor
+la	$s6,stringValorEscrever
+loopLeituraCompactar:
+	move	$a0,$s7
+	li	$v0,14
+	la	$a1,inputBuffer
+	li	$a2,1
+	syscall
+	beq	$v0,$zero,fimCompacta
+	slt	$t2,$v0,$zero
+	bne	$t2,1,continualoopLeituraCompactar	#se vo > 0, nao ha erro
+	li	$v0,4
+	la	$a0,stringErroArquivo
+	syscall
+	li	$v0,10
+	syscall	
+	continualoopLeituraCompactar:
+		lb	$t5,($a1)
+		sb	$t5,($s6)
+		addi	$sp,$sp,-8
+		sw	$s7,4($sp)
+		sw	$s6,($sp)
+		la	$a0,stringValorEscrever
+		jal	retornaIndex
+		move	$t4,$v0	#recebe o index no arquivo compactado, se for -1 nao existe
+		lw	$s6,($sp)		
+		lw	$s7,4($sp)
+		addi	$sp,$sp,8
+		bne	$t4,-1,existeByteCompactado	
+		#entra aqui se ainda nao existir
+		addi	$s6,$s6,1
+		li	$t3,'\0'
+		sb	$t3,($s6)
+		la	$t7,stringPadraoZero
+		li	$t3,'0'
+		sb	$t3,($t7)
+		addi	$t7,$t7,1
+		li	$t3,'\0'
+		sb	$t3,($t7)
+		la	$a1,stringValorEscrever
+		la	$a0,stringPadraoZero
+		addi	$sp,$sp,-8
+		sw	$s7,4($sp)
+		sw	$s6,($sp)
+		jal	addArquivo
+		lw	$s6,($sp)		
+		lw	$s7,4($sp)
+		addi	$sp,$sp,8
+		la	$s6,stringValorEscrever
+		j	loopLeituraCompactar
+		existeByteCompactado:
 
+fimCompacta:
+	li	$v0,10
+	syscall
 
-
-
-addArquivo:	#recebe $a0 valor , $a1 chave
+addArquivo:	#recebe $a0 chave , $a1 valor
 	move	$t0,$a0
 	move	$t1,$a1
-	la	$a0,fileCompactadoName
-	li	$a1,1
-	li	$a2,0
 	li	$v0,13
+	la	$a0,fileCompactadoName
+	li	$a1, 1          
+	li 	$a2, 0
 	syscall
 	slt	$t2,$v0,$zero
 	bne	$t2,1,continuaFuncaoAdd	#se vo > 0, nao ha erro
@@ -40,6 +99,7 @@ addArquivo:	#recebe $a0 valor , $a1 chave
 	syscall	
 	continuaFuncaoAdd:
 	move	$a0,$v0
+	move	$s7,$v0
 	li	$v0,15
 	la	$a1,outputBuffer
 	li	$a2,1
@@ -61,7 +121,9 @@ addArquivo:	#recebe $a0 valor , $a1 chave
 				li	$t5,')'
 				sb	$t5,($a1)
 				syscall
-				li	$v0,15
+				li	$v0,16
+				move	$a0,$s7
+				syscall
 				jr	$ra
 				escreveVirgula:
 					li	$t5,','
@@ -94,7 +156,7 @@ retornaIndex:	#recebe $a0 string a pesquisar no arquivo compactado
 		la	$s4,stringChave
 		loopLeituraArquivo:
 			move	$a0,$s7
-			la	$a1,bufferLeituraArquivo
+			la	$a1,inputBuffer
 			li	$v0,14
 			li	$a2,1
 			syscall
@@ -119,7 +181,7 @@ retornaIndex:	#recebe $a0 string a pesquisar no arquivo compactado
 				lb	$t0,($s6)
 				addi	$s6,$s6,1
 				beq	$t0,'(',pula
-				beq	$t0,'\0',pula	#mudar para \0
+				beq	$t0,'\0',pula	
 				sb	$t0,($s2)
 				addi	$s2,$s2,1
 			pula:
@@ -133,8 +195,6 @@ retornaIndex:	#recebe $a0 string a pesquisar no arquivo compactado
 				beq	$t0,'\0',fimString	#trocar esse valor por \0
 				j	inicioString
 			fimString:
-				#addi	$s4,$s4,-1		#RETIRAR LINHA
-				#sb	$zero,($s4)		#RETIRAR LINHA - servem apenas para trocar o ) por \0
 				la	$a0,stringChave
 				move	$a1,$s3
 				comparaString:
@@ -151,9 +211,15 @@ retornaIndex:	#recebe $a0 string a pesquisar no arquivo compactado
 					addi	$a1,$a1,1
 					j	comparaString
 				fimCompara:
+					move	$a0,$s7
+					li	$v0,16
+					syscall
 					move	$v0,$s5
 					jr	$ra
 				fimArquivo:
+					move	$a0,$s7
+					li	$v0,16
+					syscall
 					li	$v0,-1
 					jr	$ra
 	
